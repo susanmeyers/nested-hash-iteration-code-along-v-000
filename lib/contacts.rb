286d4b9aa0eb525a833c7feab9d430078e470a99
Susan Meyers <smmeyers24@gmail.com>
2018-08-28 02:31:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,7 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_flavors]
+  contacts["Freddy Mercury"]
+  contacts.each do |person, contact_details_hash|
   binding.pry
 end
