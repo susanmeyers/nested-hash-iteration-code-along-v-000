b817eb1b4961e5e0bfbefcab2cbdc71128e988b7
Susan Meyers <smmeyers24@gmail.com>
2018-08-28 02:38:06 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,6 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"]
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
 
