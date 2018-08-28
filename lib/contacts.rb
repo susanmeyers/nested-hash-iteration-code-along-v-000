8fbb48634cbcaf6416c1df3e27c866bfd615d2a5
Susan Meyers <smmeyers24@gmail.com>
2018-08-28 02:57:19 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,8 @@ def remove_strawberry(contacts)
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-
-  binding.pry
+        
+  #binding.pry
      end
     end
   end
